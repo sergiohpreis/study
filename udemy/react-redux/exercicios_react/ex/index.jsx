f43dc76408e5d2bccf425213923c78dda2f4ce7f
Sergio Reis <sergio.reis@pmweb.com.br>
2017-08-02 18:30:34 -0300

    Udemy - React + Redux | Olá React (Exercicios React)

---
+++ udemy/react-redux/exercicios_react/ex/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Método responsável por renderizar os componentes no DOM
+ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'));
