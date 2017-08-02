ce4f8303a7a868e5f2fb973f71ad7f04248fb69f
Sergio Reis <sergio.reis@pmweb.com.br>
2017-08-02 18:59:46 -0300

    Udemy - React + Redux | Mantendo Histórico da Aula  (Exercicios React)

---
+++ udemy/react-redux/exercicios_react/ex.3/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './component';
+
+// Método responsável por renderizar os componentes no DOM
+ReactDOM.render(<Component />, document.getElementById('app'));
