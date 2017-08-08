4853c9de1cecd461dbb4adea50ad5179300a3d79
Sergio Reis <sergio.reis@pmweb.com.br>
2017-08-08 11:21:06 -0300

    Udemy - React + Redux | Enabling CORS | #TodoApp

--- udemy/react-redux/todo-app/backend/src/config/server.js
+++ udemy/react-redux/todo-app/backend/src/config/server.js
@@ -9,12 +9,18 @@ const bodyParser = require('body-parser');
 const express = require('express');
 // Instancia do Express
 const server = express();
+// Referencia do Middleware do CORS
+const allowCors = require('./cors');
+
+// server.use são Middlewares
 
 /* Middleware responsável pelas requisições do padrão 'urlencoded' 
 (Padrão usado na submissão de formulários) */
 server.use(bodyParser.urlencoded({extended: true}));
 // Middleware que faz o parse dos JSONs
 server.use(bodyParser.json());
+// Permite o CORS
+server.use(allowCors);
 
 // Monitora a porta
 server.listen(port, () => {
