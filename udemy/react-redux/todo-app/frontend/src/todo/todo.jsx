878a08a0213951a1bb5a8a9db56710ee63929021
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-08-12 13:04:40 -0300

    Udemy - React + Redux | Event onChange - Frontend | #TodoApp

--- udemy/react-redux/todo-app/frontend/src/todo/todo.jsx
+++ udemy/react-redux/todo-app/frontend/src/todo/todo.jsx
@@ -7,18 +7,27 @@ import TodoList from './todoList';
 export default class Todo extends Component {
     constructor(props){
         super(props);
+        this.state = {description: '', list: []};
+        
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     };
 
     handleAdd() {
-        console.log(this);
+        console.log(this.state.description);
+    };
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
     };
 
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoForm 
+                handleAdd={this.handleAdd}
+                handleChange={this.handleChange}/>
                 <TodoList />
             </div>
         );
