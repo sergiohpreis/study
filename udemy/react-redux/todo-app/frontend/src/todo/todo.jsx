34f4056514b7cce546ccf5d6d4cfd5b84b7c1903
Sergio Henrique Pereira Reis <sergiohpreis@Sergios-MacBook-Pro.local>
2017-08-17 07:47:21 -0300

    Udemy - React + Redux | Adding comments - Frontend | #TodoApp

--- udemy/react-redux/todo-app/frontend/src/todo/todo.jsx
+++ udemy/react-redux/todo-app/frontend/src/todo/todo.jsx
@@ -9,8 +9,8 @@ const URL = 'http://localhost:3003/api/todos'
 
 /*
 O componente Todo será responsável por todo o estado
-da aplicação, isso pois é mais simples de espalhar esse estado
-para todos os outros componentes.
+da aplicação, pois é mais simples de espalhar esse estado
+para todos os outros componentes (usando Redux esse cenário muda)
 */
 export default class Todo extends Component {
     constructor(props){
@@ -27,6 +27,7 @@ export default class Todo extends Component {
         this.refresh();
     };
 
+    // Método responsável por trazer a lista atualizada
     refresh() {
         /*
         1 - Faz um GET organizando por data de criação
@@ -37,6 +38,7 @@ export default class Todo extends Component {
             .then(resp => this.setState({...this.state, description: '', list: resp.data}))
     };
 
+    // Método responsável por adicionar uma nova todo
     handleAdd() {
         const description = this.state.description;
 
@@ -48,11 +50,13 @@ export default class Todo extends Component {
             .then(resp => this.refresh());
     };
 
+    // Método responsável por atualizar o state  
     handleChange(e) {
         // Seta o State da descrição baseada no valor do input
         this.setState({...this.state, description: e.target.value})
     };
 
+    // Método responsável por remover um todo
     handleRemove(todo) {
         /*
         1 - Faz um DELETE utilizando o ID da TODO
@@ -62,12 +66,22 @@ export default class Todo extends Component {
             .then(result => this.refresh())
     };
 
+    // Método responsável por marcar um todo como feito
     handleMarkAsDone(todo) {
+        /*
+        1 - Faz um PUT utilizando o ID e passando o done como true
+        2 - Faz um refresh utilizando o método refresh
+        */
         axios.put(`${URL}/${todo._id}`, {...todo, done: true})
             .then(result => this.refresh())
     };
 
+    // Método responsável por marcar um todo como não feito
     handleMarkAsPending(todo) {
+        /*
+        1 - Faz um PUT utilizando o ID e passando o done como false
+        2 - Faz um refresh utilizando o método refresh
+        */
         axios.put(`${URL}/${todo._id}`, {...todo, done: false})
             .then(result => this.refresh())
     };
