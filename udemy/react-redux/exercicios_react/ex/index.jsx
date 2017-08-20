6dfeb767f0ee0b7b004a44f40c07812266c6f21b
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-08-20 17:20:25 -0300

    Udemy - React + Redux | Adicionando Comentários | #Exercices React Redux

--- udemy/react-redux/exercicios_react/ex/index.jsx
+++ udemy/react-redux/exercicios_react/ex/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/*
+combineReducers: Cada atributo do store é controlado por um reducer.
+O combineReducers vai combinar cada reducer (funções puras), apontando cada um
+para seu respectivo objeto
+createStore: Serve para criar stores (estado unico da aplicação)
+*/
 import {combineReducers, createStore} from 'redux';
+// Componente que passa o estado para os componentes internos
 import {Provider} from 'react-redux';
 
 import Field from './field';
