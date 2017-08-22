88b2496083964bb082e53f691a74a942f905044e
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-08-22 08:54:28 -0300

    Udemy - React + Redux | Integrando React-Redux - Parte 2 | #Exercices React Redux

--- udemy/react-redux/exercicios_react/ex/index.jsx
+++ udemy/react-redux/exercicios_react/ex/index.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-/*
-combineReducers: Cada atributo do store é controlado por um reducer.
-O combineReducers vai combinar cada reducer (funções puras), apontando cada um
-para seu respectivo objeto
-createStore: Serve para criar stores (estado unico da aplicação)
-*/
 import {combineReducers, createStore} from 'redux';
-// Componente que passa o estado para os componentes internos
 import {Provider} from 'react-redux';
 
-import Field from './field';
-import fieldReducer from './fieldReducer';
+import counterReducer from './counterReducer';
+import Counter from './counter';
 
 const reducers = combineReducers({
-    field: fieldReducer
+    counter: counterReducer
 });
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue='Teste' />
+        <Counter />
     </Provider>
 , document.getElementById('app'));
