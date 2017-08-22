71a14667348586ea7465d1f57e5e5bc1f2d55421
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-08-22 09:51:05 -0300

    Udemy - React + Redux | Configurando o Redux | #TodoApp - Migrando Redux

--- udemy/react-redux/todo-app-redux/frontend/src/index.jsx
+++ udemy/react-redux/todo-app-redux/frontend/src/index.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './main/app';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import App from './main/app';
+import reducers from './main/reducers';
+
+// Cria o estado único da aplicação
+const store = createStore(reducers);
+
+ReactDOM.render(
+    /**
+     * O Provider envolve toda a aplicação para
+     * passar o estado único
+     */
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('app'));
