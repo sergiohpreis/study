18b99a9891133cefc9d650efa8c868c9ac677a91
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-08-22 09:42:30 -0300

    Udemy - React + Redux | Configurando os Reducers | #TodoApp - Migrando Redux

---
+++ udemy/react-redux/todo-app-redux/frontend/src/main/reducers.js
@@ -0,0 +1,28 @@
+/**
+ * Esse arquivo que concentra todos os reducers
+ * da aplicação
+ */
+
+// Combina os reducers
+import {combineReducers} from 'redux';
+
+// Reducer que combina todos os outros reducers
+const rootReducer = combineReducers({
+    todo: () => ({
+        list: [{
+            _id: 1,
+            description: 'Pagar fatura do cartão',
+            done: true
+        }, {
+            _id: 2,
+            description: 'Reunião com a equipe as 10:00',
+            done: false
+        }, {
+            _id: 3,
+            description: 'Consulta médica na terça depois do almoço',
+            done: false
+        }]
+    })
+});
+
+export default rootReducer;
