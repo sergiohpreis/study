5781df0afd9aab1282191a0a3440a74466f54f7e
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-08-30 09:01:53 -0300

    Udemy - React + Redux | Applicando Middleware (Redux-Promise) | #TodoApp - Migrando Redux

--- udemy/react-redux/todo-app-redux/frontend/src/index.jsx
+++ udemy/react-redux/todo-app-redux/frontend/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
+import promise from 'redux-promise';
 
 import App from './main/app';
 import reducers from './main/reducers';
@@ -10,7 +11,7 @@ const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 // Cria o estado único da aplicação
-const store = createStore(reducers, devTools);
+const store = applyMiddleware(promise)(createStore)(reducers, devTools);
 
 ReactDOM.render(
     /**
