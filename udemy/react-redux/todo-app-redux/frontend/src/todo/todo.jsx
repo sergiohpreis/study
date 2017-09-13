7d305cc84338275c791817d3e352d64dc9a038b7
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-09-13 18:13:57 -0300

    Udemy - React + Redux | Melhorando a Action de Pesquisa | #TodoApp - Migrando Redux

--- udemy/react-redux/todo-app-redux/frontend/src/todo/todo.jsx
+++ udemy/react-redux/todo-app-redux/frontend/src/todo/todo.jsx
@@ -1,125 +1,13 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React from 'react';
 
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
-const URL = 'http://localhost:3003/api/todos'
-
-/*
-O componente Todo será responsável por todo o estado
-da aplicação, pois é mais simples de espalhar esse estado
-para todos os outros componentes (usando Redux esse cenário muda)
-*/
-export default class Todo extends Component {
-    constructor(props){
-        super(props);
-        this.state = {description: '', list: []};
-        
-        // Faz um bind para não perder o contexto do this
-        this.handleAdd = this.handleAdd.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleRemove = this.handleRemove.bind(this);
-        this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
-        this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
-        this.handleClear = this.handleClear.bind(this);
-    
-        this.refresh();
-    };
-
-    // Método responsável por trazer a lista atualizada
-    refresh(description = '') {
-        /*
-        Se a descrição for valida adiciona o parametro na URL do GET
-        caso contrário, deixa vazio
-        */
-        const search = description ? `&description__regex=/${description}/` : '';
-        
-        /*
-        1 - Faz um GET organizando por data de criação
-        2 - Utiliza a descrição informada (para manter a descrição no input
-        devemos passar o estado)
-        3 - Preenche a lista com todas as tarefas criadas no Banco
-        */
-        axios.get(`${URL}?sort=-createdAt${search}`)
-            .then(resp => this.setState({...this.state, description, list: resp.data}))
-    };
-
-    // Método responsável por adicionar uma nova todo
-    handleAdd() {
-        const description = this.state.description;
-
-        /*
-        1 - Faz um POST enviando a descrição
-        2 - Faz um refresh usando o método refresh
-        */
-        axios.post(URL, {description})
-            .then(resp => this.refresh());
-    };
-
-    // Método responsável por atualizar o state  
-    handleChange(e) {
-        // Seta o State da descrição baseada no valor do input
-        this.setState({...this.state, description: e.target.value})
-    };
-
-    // Método responsável por remover um todo
-    handleRemove(todo) {
-        /*
-        1 - Faz um DELETE utilizando o ID da TODO
-        2 - Faz um refresh usando o método refresh
-        */
-        axios.delete(`${URL}/${todo._id}`)
-            .then(result => this.refresh(this.state.description))
-    };
-
-    // Método responsável por marcar um todo como feito
-    handleMarkAsDone(todo) {
-        /*
-        1 - Faz um PUT utilizando o ID e passando o done como true
-        2 - Faz um refresh utilizando o método refresh
-        */
-        axios.put(`${URL}/${todo._id}`, {...todo, done: true})
-            .then(result => this.refresh(this.state.description))
-    };
-
-    // Método responsável por marcar um todo como não feito
-    handleMarkAsPending(todo) {
-        /*
-        1 - Faz um PUT utilizando o ID e passando o done como false
-        2 - Faz um refresh utilizando o método refresh
-        */
-        axios.put(`${URL}/${todo._id}`, {...todo, done: false})
-            .then(result => this.refresh(this.state.description))
-    };
-
-    // Método responsável pela busca de todos especificas
-    handleSearch() {
-        this.refresh(this.state.description);
-    };
-
-    handleClear() {
-        this.refresh();
-    }
-
-    render() {
-        return (
-            <div>
-                <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm 
-                description={this.state.description}
-                handleAdd={this.handleAdd}
-                handleChange={this.handleChange}
-                handleSearch={this.handleSearch}
-                handleClear={this.handleClear}/>
-                <TodoList 
-                list={this.state.list}
-                handleRemove={this.handleRemove}
-                handleMarkAsDone={this.handleMarkAsDone}
-                handleMarkAsPending={this.handleMarkAsPending}/>
-            </div>
-        );
-    };
-}
+export default props => (
+    <div>
+        <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+        <TodoForm/> 
+        <TodoList />
+    </div>
+);
--- udemy/react-redux/todo-app-redux/frontend/src/todo/todoForm.jsx
+++ udemy/react-redux/todo-app-redux/frontend/src/todo/todoForm.jsx
@@ -44,7 +44,7 @@ class TodoForm extends Component {
                     <IconButton style="primary" icon="plus"
                         onClick={() => add(description)}></IconButton>
                     <IconButton style="info" icon="search"
-                        onClick={() => search()}/>
+                        onClick={search}/>
                     <IconButton style="default" icon="close"
                         onClick={this.props.clear}/>
                 </Grid>
