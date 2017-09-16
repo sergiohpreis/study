a78ad774e657407969ad680e65934986e062582b
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-09-16 11:08:44 -0300

    Udemy - React + Redux | Criação da API REST | #MyMoneyApp

---
+++ udemy/react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,9 @@
+const BillingCycle = require('./billingCycle');
+
+// Métodos que serão atendidos pela aplicação
+BillingCycle.methods(['get', 'post', 'put', 'delete']);
+/**
+ * new - Retorna o objeto novo ao ser feito um update
+ * runValidators - Executará as validações no update
+ */
+BillingCycle.updateOptions({new: true, runValidators: true});
