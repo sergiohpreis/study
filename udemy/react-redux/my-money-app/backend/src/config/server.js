4e26df27036502991c26efaaa771448809a4878b
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-09-17 12:39:19 -0300

    Udemy - React + Redux | Middleware Query Parser Int | #MyMoneyApp

--- udemy/react-redux/my-money-app/backend/src/config/server.js
+++ udemy/react-redux/my-money-app/backend/src/config/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
 const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 
 /**
  * Aplicando os middlewares, que são responsáveis por
@@ -12,6 +13,8 @@ const allowCors = require('./cors');
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 server.use(allowCors);
+// Transforma as requisições numéricas em inteiros
+server.use(queryParser());
 
 server.listen(port, () => {
     console.log(`BACKEND is running on port ${port}`);
