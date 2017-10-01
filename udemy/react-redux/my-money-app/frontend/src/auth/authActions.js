a0ffb01cce728ea7f800a0e6312bb2e25e692f21
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-10-01 20:17:41 -0300

    Udemy - React + Redux | Inserindo action validateToken | #MyMoneyApp

--- udemy/react-redux/my-money-app/frontend/src/auth/authActions.js
+++ udemy/react-redux/my-money-app/frontend/src/auth/authActions.js
@@ -5,6 +5,17 @@ import consts from '../consts';
 export const login = values => submit(values, `${consts.OAPI_URL}/login`);
 export const signup = values => submit(values, `${consts.OAPI_URL}/signup`);
 export const logout = () => ({type: 'TOKEN_VALIDATED', payload: false});
+export const validateToken = token => dispatch => {
+    if (token) {
+        axios.post(`${consts.OAPI_URL}/validateToken`, {token})
+            .then(resp => {
+                dispatch({type: 'TOKEN_VALIDATED', payload: resp.data.valid});
+            })
+            .catch(e => dispatch({type: 'TOKEN_VALIDATED', payload}));
+    } else {
+        dispatch({type: 'TOKEN_VALIDATED', payload});
+    };
+};
 
 const submit = (values, url) => {
     return dispatch => {
